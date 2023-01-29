8791f509d9e01edc79a15af828683323d8357d67
Adrian Moreira <adrianmoreira975@gmail.com>
2023-01-29 15:47:17 -0600

    Dogs can be pet

--- JameGam2023-Glitch/Assets/Resources/Scripts/GameOver.cs
+++ JameGam2023-Glitch/Assets/Resources/Scripts/GameOver.cs
@@ -13,6 +13,11 @@ public class GameOver : MonoBehaviour
         instance = this;
     }
 
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
     public void openGameOver()
     {
         gameObject.SetActive(true);
@@ -26,13 +31,7 @@ public class GameOver : MonoBehaviour
 
     public void quitGame()
     {
-#if UNITY_EDITOR
-        // Application.Quit() does not work in the editor so
-        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-         Application.Quit();
-#endif
+        TitleScreen.instance.resetTitle();
     }
 
 
